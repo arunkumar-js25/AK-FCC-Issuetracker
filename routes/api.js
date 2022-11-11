853ea4977053e81c48a9e00c170abbff1d09c23f
Arun Kumar <>
2022-11-11 02:38:30 +0000

    POST testcases

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,22 @@
 'use strict';
+require('dotenv').config();
+const URI = process.env.MONGO_URI;
+let mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+let issueSchema = new mongoose.Schema({
+    issue_title : { type: String },
+    issue_text: { type: String },
+    created_on: { type: Date },
+    updated_on: { type: Date },
+    created_by: { type: String },
+    assigned_to: { type: String },
+    open:{ type: Boolean },
+    status_text: { type: String },
+  });
+
+//CREATE INSTANCE OF A MODEL
+let Issue = mongoose.model('Issue', issueSchema);
 
 module.exports = function (app) {
 
@@ -11,7 +29,30 @@ module.exports = function (app) {
     
     .post(function (req, res){
       let project = req.params.project;
+      console.log(project);
       
+      let issueData = req.body;
+
+      if(issueData.issue_title == undefined || 
+        issueData.issue_text == undefined ||
+        issueData.created_by == undefined ){
+        return res.json({ error: 'required field(s) missing' });
+      }
+      
+      issueData.open=true;
+      issueData.created_on=new Date();
+      issueData.updated_on=new Date();
+      
+      let IssueDetail = new Issue(issueData);
+  		IssueDetail.save(function(err, data) {
+  			if (err) 
+        {
+          console.error(err); 
+          return res.json({});
+        }
+        //console.log(issueData);
+  			return res.json(issueData);
+  		});
     })
     
     .put(function (req, res){
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,49 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-  
+  test('an issue with all fields',function(done){
+    chai.request(server)
+    .post('/api/issues/apitest')
+    .send({"issue_title":"unix",
+           "issue_text":"testing for projectname",
+           "created_by":"user",
+           "assigned_to":"Arun",
+           "status_text":"Critical"})
+    .end(function(err, res){
+      if(err){
+        console.error(err);
+      }
+      assert.equal(res.body.issue_title,'unix');
+      done();
+    })
+  });
+
+  test('issue with only required fields',function(done){
+    chai.request(server)
+    .post('/api/issues/apitest')
+    .send({"issue_title":"unix",
+           "issue_text":"testing for projectname",
+           "created_by":"user"})
+    .end(function(err, res){
+      if(err){
+        console.error(err);
+      }
+      assert.equal(res.body.issue_title,'unix');
+      done();
+    })
+  });
+
+  test('Create an issue with missing required fields',function(done){
+    chai.request(server)
+    .post('/api/issues/apitest')
+    .send({"issue_title":"unix",
+           "issue_text":"testing for projectname"})
+    .end(function(err, res){
+      if(err){
+        console.error(err);
+      }
+      assert.equal(res.body.error,'required field(s) missing');
+      done();
+    })
+  })
 });
